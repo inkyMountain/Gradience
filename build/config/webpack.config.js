eee5a4a7f060563a2027eb6629c76d390530587f
chenyitao <285345240@qq.com>
2019-12-19 23:03:02 +0800

    configure webpack

--- build/config/webpack.config.js
+++ build/config/webpack.config.js
@@ -1,20 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: path.resolve(__dirname, '../../', 'src/index.tsx'),
+
+  // resolve: {
+  //   extensions: ['js', 'jsx', 'ts', 'tsx']
+  // },
 
   module: {
     rules: [
+      //  transpile ts/tsx to js
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader',
-        include: path.resolve(__dirname, 'src')
       }
     ]
   },
 
   output: {
     filename: 'main.[chunkhash].js',
-    publicPath: '/assets/'
-  }
+    path: path.resolve(__dirname, '../../', 'dist'),
+    // umd is both compatiable for nodejs and browser
+    libraryTarget: 'umd'
+  },
+
+  devServer: {
+    port: 7000
+  },
+
+  plugins: [
+    //  auto inject .js file to template.html
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../../', 'template.html'),
+    })
+  ]
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
-console.log('entry run')
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const button = <button
+  onClick={() => console.log('button clicked')}>点击后打印</button>;
+ReactDom.render(button, document.querySelector('#root'));
+console.log('entry run');
