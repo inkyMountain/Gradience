796bb31bcca3f723585fdbd8f93ccb9573968f22
chenyitao <285345240@qq.com>
2019-12-29 17:51:47 +0800

    configure jest-junit

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -1,6 +1,12 @@
 const base = require('./jest.config');
 const config = Object.assign({}, base, {
   collectCoverage: false,
-  reporters: ['default', 'jest-junit'],
+  reporters: [
+    'default',
+    ['jest-junit', {
+      'outputDirectory': './test-results/jest',
+      'outputName': 'results.xml'
+    }]
+  ],
 });
 module.exports = config;
