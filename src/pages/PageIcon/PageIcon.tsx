136e33f7ab0b0b089b2d49ee4071f38ac2ccfed4
chenyitao <chet.chen@jollycorp.com>
2019-12-31 09:26:21 +0800

    add pages directory into track

---
+++ src/pages/PageIcon/PageIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Icon from '../../components/Icon/Icon';
+
+const PageIcon = () => {
+  return <div className={'page-icon'}>
+    <Icon name='wechat'/>
+    <Icon name='alipay'/>
+    <Icon name='right'/>
+  </div>;
+};
+
+export default PageIcon;
+
